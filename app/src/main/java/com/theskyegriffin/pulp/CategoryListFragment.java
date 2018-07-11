5a4708efdff7ea85e2e1fb0a8a08dcc1f3f9c16f
skybox <skybox@trioptimum.com>
2018-07-11 05:21:24 -0500

    Adding a RecyclerView back to the CategoryList fragment.

--- app/src/main/java/com/theskyegriffin/pulp/CategoryListFragment.java
+++ app/src/main/java/com/theskyegriffin/pulp/CategoryListFragment.java
@@ -1,7 +1,10 @@
 package com.theskyegriffin.pulp;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +18,29 @@ public class CategoryListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_category_list, container, false);
+        Context context = getContext();
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.rv_categories);
+        String[] categories = new String[] {
+                "Food",
+                "Electricity",
+                "Rent",
+                "Car Maintenance",
+                "Food",
+                "Electricity",
+                "Rent",
+                "Car Maintenance",
+                "Food",
+                "Electricity",
+                "Rent",
+                "Car Maintenance",
+                "Food",
+                "Electricity",
+                "Rent",
+                "Car Maintenance",
+        };
+        CategoryListAdapter categoryAdapter = new CategoryListAdapter(categories);
+        recyclerView.setAdapter(categoryAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
 
         return view;
     }
--- app/src/main/java/com/theskyegriffin/pulp/MainActivity.java
+++ app/src/main/java/com/theskyegriffin/pulp/MainActivity.java
@@ -1,11 +1,9 @@
 package com.theskyegriffin.pulp;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] categories = new String[] {
-                "Food",
-                "Electricity",
-                "Rent",
-                "Car Maintenance"
-        };
 
         ViewPager viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(new PulpFragmentPagerAdapter(getSupportFragmentManager()));
@@ -26,9 +18,5 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
 
-//        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_categories);
-//        CategoryListAdapter categoryAdapter = new CategoryListAdapter(categories);
-//        recyclerView.setAdapter(categoryAdapter);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
